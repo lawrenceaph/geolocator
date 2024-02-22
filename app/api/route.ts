7d4f160b4dd3c6c7ffe1dfeaed85d53bdc3fdc2a
Lawrence Aritao <lawrence.aritao@gmail.com>
2024-02-22 23:03:44 +0000

    cf ipcountry included

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,8 +3,11 @@ import { geolocation } from '@vercel/edge';
 export const runtime = 'edge';
  
 export function GET(request: Request) {
+    const sourcecountry = request.headers.get('cf-ipcountry');
   const { country } = geolocation(request);
-  return new Response(`<h1>Your location is ${country}</h1>`, {
+  return new Response(`<h1>Your location is ${country}</h1> but source country is ${sourcecountry}`, {
     headers: { 'content-type': 'text/html' },
   });
 }
+
+
