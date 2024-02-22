f75a3fb6ff933f4f9b968aef2f22ef67db0398f3
Lawrence Aritao <lawrence.aritao@gmail.com>
2024-02-22 23:15:32 +0000

    test redirect for IN visitors

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse, NextRequest } from 'next/server';
+import { geolocation } from '@vercel/edge';
+ 
+export const runtime = 'edge';
+// This function can be async if you need to perform asynchronous tasks
+export default function middleware(request: NextRequest) {
+  const { nextUrl: url, geo } = request
+  const country =  request.headers.get('cf-ipcountry'); 
+  
+  console.log("visitor from ", country)
+  if (country === 'IN') {
+
+    return NextResponse.redirect(new URL('https://ijmindia.org', request.url));
+  }  
+
+
+  // For any other country, or if the country code isn't available, continue with the request
+  return NextResponse.next();
+}
